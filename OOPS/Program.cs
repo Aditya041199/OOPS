617d98724a9e6b761a94570bc1eb6e5d7e0e457a
aditya patil <Aditya@example.com>
2022-11-16 07:49:17 +0530

    [Aditya]Add:Inventory Details Management

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOPS.InventoryManagement;
+using System;
+namespace OOPS
+{
+    public class Program
+    {
+        public static string inventoryFilePath = @"D:\Bridgelabz\OOPS\OOPS\InventoryManagement\Inventory.json";
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Oops JsonFile");
+            bool condition = true;
+            while (condition)
+            {
+                Console.WriteLine("\nWhat do you want to do?\n1. InventoryDetails\n" +
+                                  "2. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        InventoryDetailsManagement inventoryDetailsManagement = new InventoryDetailsManagement();
+                        inventoryDetailsManagement.ReadJsonFile(inventoryFilePath);
+                        break;
+                    default:
+                        condition = false;
+                        break;
+                }
+            }
+        }
+    }
+}
